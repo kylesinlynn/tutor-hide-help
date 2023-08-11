e713cfeed5a6820a5f4162bbe86b025383eada73
Kyle Sin Lynn <ks.lynn@mit.com.mm>
2023-08-11 14:51:24 +0630

    fix: hide help link in studio

--- hide-help.py
+++ hide-help.py
@@ -2,13 +2,9 @@ from tutor import hooks
 
 hooks.Filters.ENV_PATCHES.add_item(
     (
-        "openedx-lms-common-settings",
-        "FEATURES['ENABLE_HELP_LINK'] = False"
-    )
-)
-hooks.Filters.ENV_PATCHES.add_item(
-    (
-        "openedx-cms-common-settings",
-        "FEATURES['ENABLE_HELP_LINK'] = False"
+        "common-env-features",
+        """
+"ENABLE_HELP_LINK": false
+"""
     )
 )
